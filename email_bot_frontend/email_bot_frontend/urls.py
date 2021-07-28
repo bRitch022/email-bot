c0e8c7131f7dee14642c34782c2bdd71c3cafaa1
PugLoverForLife <devinhimmelheber@gmail.com>
2021-07-28 12:47:36 -0600

    Single line html page
    Has one page that is the home page with a single line of text.

--- email_bot_frontend/email_bot_frontend/urls.py
+++ email_bot_frontend/email_bot_frontend/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #path('form/', include('bot_form.urls')), # For when the initial page is a sign up/in page
+    path('', include('bot_form.urls')),
 ]
