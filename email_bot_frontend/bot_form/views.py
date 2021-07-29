cd8f5ae09fbf29d698f7bfdf88851d14fc38b5f7
PugLoverForLife <devinhimmelheber@gmail.com>
2021-07-28 22:30:35 -0600

    Sending dummy data to display on screen
    uses render function to pass a list to the html function

--- email_bot_frontend/bot_form/views.py
+++ email_bot_frontend/bot_form/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+bot_data = [
+        "devinhimmelheber@gmail.com",
+        "devinhimmelheber1@gmail.com",
+        "What up fuckers?!",
+        "That's right, dummy data for the win!",
+]
+to_email = "devinhimmelheber@gmail.com"
+from_email = "devinhimmelheber1@gmail.com"
+subject = "What up fuckers?!"
+body_text = "That's right, dummy data for the win!"
+
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Bot Form</h1>')
+    context = {
+        'data': bot_data
+    }
+    return render(request, 'bot_form/form.html', context)
