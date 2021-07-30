0919ee288492c4f6b19c811f0643c199c8d94768
PugLoverForLife <devinhimmelheber@gmail.com>
2021-07-29 21:15:38 -0600

    Can create email bots and user sign in functionality

--- email_bot_frontend/bot_form/urls.py
+++ email_bot_frontend/bot_form/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import BotListView, BotCreateView, BotUpdateView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='form-home'),
+    path('', BotListView.as_view(), name='form-home'),
+    path('create_bot/', BotCreateView.as_view(), name='bot-create'),
+    #path('update_bot/<int:pk>', BotUpdateView.as_view(), name='bot-update'),
 ]
--- email_bot_frontend/email_bot_frontend/settings.py
+++ email_bot_frontend/email_bot_frontend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bot_form.apps.BotFormConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -125,3 +126,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'form-home'
+LOGIN_URL = 'login'
